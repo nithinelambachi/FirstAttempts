8d92c42297f1ec3c797ff8a945cf452c324d7d74
Nithin P <nithinelambachi@gmail.com>
2021-11-12 23:01:28 +0530

    updated for dynamic datas

--- JS/array.js
+++ JS/array.js
@@ -1,14 +1,19 @@
-let input=[ ["name", "age"], ["John", 22], ["David", 30] ] ;
+let input=[ ["name", "age", "salary"], ["John", 22, 56000], ["David", 30, 46800] ] ;
 // const header = input[0];
 // const value=input.slice(1);
 let [header, ...value]=input;
-// console.log(header);
-// console.log(value);
+
+console.log(header);
+console.log(value);
 const result =value.map((value)=>{
     const obj={};
-    obj[header[0]]=value[0];
-    obj[header[1]]=value[1];
+    for(let i=0; i<header.length; i++){
+        obj[header[i]]=value[i];
+    }
     return obj;
+    // obj[header[0]]=value[0];
+    // obj[header[1]]=value[1];
+
 });
    
 console.log(result);
